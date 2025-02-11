acefa1371410de755e55c5dbaa759e1d8911883e
yoshiyuki-takahashi-dm <yoshiyuki.takahashi@konicaminolta.com>
2025-02-11 23:21:07 +0900

    トレーナーごとページのコンテンツを追加

--- pages/trainer/[name]/index.vue
+++ pages/trainer/[name]/index.vue
@@ -8,8 +8,32 @@ console.log("Trainer: " + trainer);
 
 <template>
   <div>
+    <!-- タイトル　トレーナー名表示 -->
     <h1>やあ {{ trainer }} ！　ぼうけん　を　はじめよう</h1>
-    <!-- ここにトレーナーごとのコンテンツを追加 -->
+
+    <!-- アバター画像 -->
+    <div>
+      <img src="/avatar.png" alt="Avatar" class="trainer-img">
+    </div>
+
+    <!-- トレーナー削除ボタン -->
+    <GamifyButton >マサラタウンにかえる（未実装）</GamifyButton>
+
+    <!-- てもちぽけもん タイトル-->
+    <h1>てもちポケモン</h1>
+
+    <!-- ぽけもんげっとボタン -->
+    <GamifyButton src="pokemon_ball.svg" alt="Icon">
+      ポケモンをつかまえる</GamifyButton>
+
+    <!-- てもちぽけもん リスト-->
+    <GamifyList>
+      <!-- <GamifyItem v-for="trainer in trainers" :key="trainer">
+        <NuxtLink :to="`/trainer/${trainer}`">{{ trainer }}</NuxtLink>
+      </GamifyItem> -->
+    </GamifyList>
+
+
   </div>
 </template>
 
