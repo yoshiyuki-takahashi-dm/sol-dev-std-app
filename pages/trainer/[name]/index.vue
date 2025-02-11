b0643fc81833bb537bb39f11ca92108321fcf50d
yoshiyuki-takahashi-dm <yoshiyuki.takahashi@konicaminolta.com>
2025-02-11 23:09:30 +0900

    トレーナー選択に対応する動的ルートフォルダを定義。選択したトレーナー名を表示するようページを仮実装

---
+++ pages/trainer/[name]/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const router = useRoute();
+const trainer = router.params.name; // パスパラメータからnameを取得
+console.log("Trainer: " + trainer);
+</script>
+
+<template>
+  <div>
+    <h1>やあ {{ trainer }} ！　ぼうけん　を　はじめよう</h1>
+    <!-- ここにトレーナーごとのコンテンツを追加 -->
+  </div>
+</template>
+
+<style scoped>
+</style>
