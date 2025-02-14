79d44d694a7aad6230c833af6566b025efd9fbac
yoshiyuki-takahashi-dm <yoshiyuki.takahashi@konicaminolta.com>
2025-02-15 00:34:38 +0900

    ポケモン一覧取得APIを実装。ローカルだと取得できないのでAWS環境で見てみる

--- pages/trainer/[name]/catch.vue
+++ pages/trainer/[name]/catch.vue
@@ -1,9 +1,35 @@
 <script setup>
 
+// ポケモンリスト
+const pokemons = ref([]);
+
+// ローディング中かどうか
+const loading = ref(true);
+
+// エラーがあったかどうか
+const error = ref(null);
+
+const fetchPokemons = async () => {
+  try {
+    const { data } = await useFetch('https://pokeapi.co/api/v2/pokemon/');
+    pokemons.value = data.results;
+} catch (err) {
+    error.value = err;
+  } finally {
+    loading.value = false;
+    console.log("Pokemons: " + pokemons.value);
+  }
+};
+
+fetchPokemons;
+
+
 </script>
 
 <template>
  <h1>ポケモンをつかまえる画面だよ</h1>
+ <div v-if="loading">読み込み中...</div>
+    <div v-else-if="error">エラーが発生しました: {{ error.message }}</div>
 </template>
 
 <style scoped>
