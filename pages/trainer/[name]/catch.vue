b803be47ad05bfbdd196157fea152909d1837a8c
yoshiyuki-takahashi-dm <yoshiyuki.takahashi@konicaminolta.com>
2025-02-17 17:06:29 +0900

    ポケモン一覧のスタイルを整えた。カスタムコンポーネント配下の指定スタイルclassにスタイルを適用する:deepがポイント

--- pages/trainer/[name]/catch.vue
+++ pages/trainer/[name]/catch.vue
@@ -10,7 +10,7 @@ var pokemonsWithDetailsList = ref([]);
 var types = null;
 var pokemonFrontDefaultImg = null;
 var pokemonBackDefaultImg = null;
-var pokemonsWithDetailsListLength = ref(0); 
+var pokemonsWithDetailsListLength = ref(0);
 
 // ローディング中かどうか
 const loading = ref(true);
@@ -51,16 +51,15 @@ finally {
     // ただ取得しただけの一覧
     pokemons = data.data;
 
-    // 型チェック
-    // console.log('useFetch返却値.resultsは' + typeof pokemons.value.results + 'です');
-
-    // pokemonsはリアクティブじゃないのにvalueプロパティを介すのはなぜだろう
-    // console.log(pokemons.value.results);
-
-    // // // ポケモン名だけを取り出せるか確認用
-    // pokemons.value.results.forEach((pokemon) => {
-    //     console.log(pokemon.name);
-    // });
+    var tmpInitialList = [];
+    for (const pokemonResult of pokemons.value.results) {
+        // 一覧情報と詳細情報を結合してtmpリストに追加
+        tmpInitialList.push({
+            ...pokemonResult,
+        });
+    }
+    // ポケモン詳細情報リストを更新
+    pokemonsWithDetailsList.value = tmpInitialList;
 
     if (pokemons == null) {
         console.log("ポケモンデータ取得できませんでした");
@@ -70,44 +69,47 @@ finally {
 }
 
 const getPokemonDetails = async () => {
+    // 更新前にクリア
+    pokemonsWithDetailsList.value = [];
+
     // ポケモン詳細情報取得
     try {
-    // 各ポケモンの詳細情報を取得して追加
-    console.log("各ポケモンの詳細情報を取得して追加")
-    var tmpList = [];
-    for (const pokemonResult of pokemons.value.results) {
-        console.log("URL: " + pokemonResult.url);
-        // ポケモンの詳細情報取得
-        const response = await fetch(
+        // 各ポケモンの詳細情報を取得して追加
+        console.log("各ポケモンの詳細情報を取得して追加")
+        var tmpList = [];
+        for (const pokemonResult of pokemons.value.results) {
+            console.log("URL: " + pokemonResult.url);
+            // ポケモンの詳細情報取得
+            const response = await fetch(
                 // URL
                 pokemonResult.url,
-        )
-        const detail = await response.json();
-        console.log("単純fetchのresponseをjson変換したもの: " + detail);
+            )
+            const detail = await response.json();
+            console.log("単純fetchのresponseをjson変換したもの: " + detail);
 
-        // 取得した詳細情報から、タイプ、画像（前後）を取得
-        types = detail.types;
-        pokemonFrontDefaultImg = detail.sprites.front_default;
-        pokemonBackDefaultImg = detail.sprites.back_default;
+            // 取得した詳細情報から、タイプ、画像（前後）を取得
+            types = detail.types;
+            pokemonFrontDefaultImg = detail.sprites.front_default;
+            pokemonBackDefaultImg = detail.sprites.back_default;
 
-        // 一覧情報と詳細情報を結合してtmpリストに追加
-        tmpList.push({
-            ...pokemonResult,
-            types,
-            imgFront: pokemonFrontDefaultImg,
-            imgBack: pokemonBackDefaultImg,
-        });
-        pokemonsWithDetailsListLength.value = tmpList.length;
-    }
+            // 一覧情報と詳細情報を結合してtmpリストに追加
+            tmpList.push({
+                ...pokemonResult,
+                types,
+                imgFront: pokemonFrontDefaultImg,
+                imgBack: pokemonBackDefaultImg,
+            });
+            pokemonsWithDetailsListLength.value = tmpList.length;
+        }
 
-    // ポケモン詳細情報リストを更新
-    pokemonsWithDetailsList.value = tmpList;
+        // ポケモン詳細情報リストを更新
+        pokemonsWithDetailsList.value = tmpList;
 
-    console.log("詳細情報: " + pokemonsWithDetailsList.value);
+        console.log("詳細情報: " + pokemonsWithDetailsList.value);
     }
     catch (err) {
         error.value = err;
-    }finally {
+    } finally {
         // なにか最後にやりたい処理があればここに書く
     }
 }
@@ -125,22 +127,29 @@ const getPokemonDetails = async () => {
         <!-- ポケモン詳細情報を取得するボタン -->
         <GamifyButton @click="getPokemonDetails()">詳細情報を取得</GamifyButton>
 
-        <h2>詳細情報を追加したポケモン一覧　確認用</h2>
-        <GamifyList>
-            <li v-for="(pokemonWithDetails, id) in pokemonsWithDetailsList" :key="id">
-                <img :src="pokemonWithDetails.imgFront" alt="ポケモンフロント画像">
-                <img :src="pokemonWithDetails.imgBack" alt="ポケモンバック画像">
-                {{ pokemonWithDetails }}
-            </li>
-        </GamifyList>
-        
         <h2>ポケモン一覧</h2>
+        <GamifyList>
+            <GamifyItem v-for="(pokemonWithDetails, id) in pokemonsWithDetailsList" :key="id">
+                <div v-if="pokemonWithDetails.imgFront">
+                    <img :src="pokemonWithDetails.imgFront" alt="ポケモンフロント画像">
+                    <img :src="pokemonWithDetails.imgBack" alt="ポケモンバック画像">
+                </div>
+                <!-- GamifyItemの中でのみpokemon-nameスタイルクラスが有効 -->
+                <span class="pokemon-name">
+                    {{ pokemonWithDetails.name }}
+                </span>
+                <GamifyButton>つかまえる</GamifyButton>
+                {{ pokemonWithDetails }}
+            </GamifyItem>
+        </GamifyList>
+
+        <!-- <h2>ポケモン一覧</h2>
         <GamifyList>
             <li v-for="(pokemon, id) in pokemons.results" :key="id">
                 {{ pokemon.name }}
                 <GamifyButton>つかまえる</GamifyButton>
             </li>
-        </GamifyList>
+        </GamifyList> -->
 
     </div>
 </template>
